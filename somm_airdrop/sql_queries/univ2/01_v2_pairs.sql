93c80f0f696d1b4775704b4357963c80ecbe768f
Sunand Raghupathi <sunraghupathi@gmail.com>
2021-11-11 17:50:42 -0500

    Bigquery for getting all uniV3 mints/burns

--- somm_airdrop/sql_queries/univ2/01_v2_pairs.sql
+++ somm_airdrop/sql_queries/univ2/01_v2_pairs.sql
@@ -20,7 +20,10 @@ CREATE TABLE uniswap_v2_pairs
 AS
 (
     SELECT
+        PARSE_V2_CREATE_LOG(logs.data, logs.topics).token0 AS token0,
+        PARSE_V2_CREATE_LOG(logs.data, logs.topics).token1 AS token1,
         PARSE_V2_CREATE_LOG(logs.data, logs.topics).pair AS pair
+
     FROM `bigquery-public-data.crypto_ethereum.logs` AS logs
     WHERE address = '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f'
     AND topics[SAFE_OFFSET(0)] = '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9'
