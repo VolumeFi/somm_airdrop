d864fd55fe007d137d7a51358892e2d3320e1def
Sunand Raghupathi <sunraghupathi@gmail.com>
2021-11-11 14:48:28 -0500

    Consolidate mints/burns + address to one query

--- somm_airdrop/sql_queries/univ2/02_v2_burns.sql
+++ somm_airdrop/sql_queries/univ2/02_v2_burns.sql
@@ -13,7 +13,7 @@ CREATE TEMP FUNCTION
 
 
 
-CREATE TABLE uniswap_v2_burns_without_sender AS (
+CREATE TABLE uniswap_v2_burns AS (
 SELECT
     logs.block_timestamp AS block_timestamp
      ,logs.block_number AS block_number
@@ -21,10 +21,12 @@ SELECT
      ,PARSE_BURN(logs.data, logs.topics).amount0 AS amount0
      ,PARSE_BURN(logs.data, logs.topics).amount1 AS amount1
      ,address as pair
+     ,transactions.from_address
 FROM `bigquery-public-data.crypto_ethereum.logs` AS logs
          JOIN uniswap_v2_pairs AS pairs ON logs.address = pairs.pair
     --  Filter for only `Burn` events
     AND topics[SAFE_OFFSET(0)] = '0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496'
+    JOIN `bigquery-public-data.crypto_ethereum.transactions` AS transactions ON transactions.hash = logs.transaction_hash
 );
 
 -- END;
