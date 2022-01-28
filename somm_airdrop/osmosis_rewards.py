c39be9445e71990e7a280d029ecc24eaf85ee0fc
Sunand Raghupathi <sunraghupathi@gmail.com>
2022-01-28 18:27:06 -0500

    Save Osmosis rewards in usomm

--- somm_airdrop/osmosis_rewards.py
+++ somm_airdrop/osmosis_rewards.py
@@ -13,10 +13,12 @@ from utils import plot_utils
 
 from typing import Any, Dict, List, TypedDict
 
-REWARD_PER_POOL = 200000 # in units of SOMM
+REWARD_PER_POOL = 200000  # in units of SOMM
+USOMM_FACTOR = 1e6
 # REWARD_PER_POOL = REWARD_PER_POOL * 1e6 # in units of uSOMM
 
 Wallet = str
+
 
 class Account(TypedDict):
     """An Osmosis account.
@@ -30,9 +32,11 @@ class Account(TypedDict):
     balance: List[dict]
     amount: str
 
+
 PoolName = str
 PoolAddress = str
-PoolShareMap = Dict[PoolName, float] 
+PoolShareMap = Dict[PoolName, float]
+
 
 class OsmosisData(TypedDict):
     """A snapshot JSON for the Osmosis pools in SIPS-002.  
@@ -42,9 +46,10 @@ class OsmosisData(TypedDict):
         pool_token_counter: Dict[PoolName, dict]
         accounts: Dict[Wallet, Account]
     """
-    num_accounts: int 
+    num_accounts: int
     pool_token_counter: Dict[PoolName, dict]
     accounts: Dict[Wallet, Account]
+
 
 class OsmosisSnapshotRewards:
     """
@@ -59,12 +64,15 @@ class OsmosisSnapshotRewards:
     """
 
     json_save_path = Path(
-        '../token_rewards/osmosis_pool_rewards.json').resolve()
+        '../token_rewards/old_rewards_in_units_somm/osmosis_pool_rewards.json'
+    ).resolve()
+    usomm_json_save_path = Path(
+        '../token_rewards/osmosis_pool_rewards_usomm.json')
 
     def __init__(self):
         self.osmosis_data: OsmosisData = self.load_snapshot_data()
-        self.wallet_to_account: Dict[Wallet, Account] = (
-            self.osmosis_data["accounts"])
+        self.wallet_to_account: Dict[Wallet,
+                                     Account] = (self.osmosis_data["accounts"])
         self.pool_to_liquidity_map: Dict[PoolName, int] = (
             self.get_pool_to_liquidity_map(
                 wallet_to_account=self.wallet_to_account))
@@ -83,15 +91,15 @@ class OsmosisSnapshotRewards:
         with open(data_path, "r") as f:
             osmosis_data: OsmosisData = json.load(f)
         return osmosis_data
-    
-    def get_pool_to_liquidity_map(self, 
-                               wallet_to_account: Dict[Wallet, Account]
-                               ) -> Dict[PoolName, int]:
+
+    def get_pool_to_liquidity_map(
+            self, wallet_to_account: Dict[Wallet,
+                                          Account]) -> Dict[PoolName, int]:
         pool_to_liquidity_map = {}
         for wallet_address, account in wallet_to_account.items():
             for pool_dict in account["balance"]:
                 pool_name = pool_dict["denom"]
-                
+
                 if pool_name in pool_to_liquidity_map:
                     pool_to_liquidity_map[pool_name]['pool_total'] += int(
                         pool_dict["amount"])
@@ -104,14 +112,15 @@ class OsmosisSnapshotRewards:
                     pool_to_liquidity_map[pool_name]['coin_total'] = int(
                         pool_dict["amount"])
         return pool_to_liquidity_map
-    
+
     @property
     def num_pools(self) -> int:
-        pool_totals = [int(v['pool_total']) 
-                       for v in self.pool_to_liquidity_map.values()]
+        pool_totals = [
+            int(v['pool_total']) for v in self.pool_to_liquidity_map.values()
+        ]
         num_pools: int = len(pool_totals)
         return num_pools
-    
+
     def map_wallets_to_pool_share_maps(self) -> Dict[Wallet, PoolShareMap]:
         # Maps wallet address to a dictionary of pool shares
         wallet_to_pool_share_map: Dict[Wallet, PoolShareMap] = {}
@@ -125,21 +134,22 @@ class OsmosisSnapshotRewards:
                 wallet_tokens_in_pool: int = int(pool_amount['amount'])
                 total_tokens_in_pool: int = int(self.pool_to_liquidity_map[
                     pool_amount['denom']]['pool_total'])
-                pool_share: float = (
-                    wallet_tokens_in_pool / total_tokens_in_pool)
+                pool_share: float = (wallet_tokens_in_pool /
+                                     total_tokens_in_pool)
                 wallet_to_pool_share_map[wallet_address][
                     pool_amount['denom']] = pool_share
         return wallet_to_pool_share_map
-    
+
     def compute_wallet_reward_map(self) -> Dict[Wallet, float]:
         wallet_reward_map: Dict[Wallet, float] = {}
         wallet_address: str
         pool_shares: Dict[PoolName, float]
-        for wallet_address, pool_shares in self.wallet_to_pool_share_map.items():
+        for wallet_address, pool_shares in self.wallet_to_pool_share_map.items(
+        ):
             wallet_reward_map[wallet_address] = 0
             for pool_name, wallet_pool_share in pool_shares.items():
-                wallet_reward_map[wallet_address] += (
-                    wallet_pool_share * REWARD_PER_POOL)
+                wallet_reward_map[wallet_address] += (wallet_pool_share *
+                                                      REWARD_PER_POOL)
         return wallet_reward_map
 
     def compute_redistribution_amounts(self):
@@ -150,12 +160,12 @@ class OsmosisSnapshotRewards:
             if wallet_reward > 50000:
                 total_redistribution_amount += wallet_reward - 50000
                 self.wallet_reward_map[wallet_address] = 50000
-        
+
         num_wallets: int = len(self.wallet_reward_map)
         redistribution_amount = total_redistribution_amount / num_wallets
         for wallet_address in self.wallet_reward_map:
             self.wallet_reward_map[wallet_address] += redistribution_amount
-        
+
         # Chop off decimals because uSOMM is the smallest unit
         # for wallet, reward in self.wallet_reward_map.items():
         #     self.wallet_reward_map[wallet] = reward
@@ -166,17 +176,39 @@ class OsmosisSnapshotRewards:
             json.dump(self.wallet_reward_map, fp)
 
         plot_utils.plot_reward_distribution(
-            wallet_to_reward=self.wallet_reward_map, 
-            save_path=Path("../plots/osmosis_pool_rewards_usomm.png").resolve(), 
+            wallet_to_reward=self.wallet_reward_map,
+            save_path=Path(
+                "../plots/osmosis_pool_rewards_usomm.png").resolve(),
             title="Osmosis LP SOMM Rewards")
+
+    def convert_to_usomm(self):
+        usomm_wallet_reward_map = {}
+        for k, v in self.wallet_reward_map.items():
+            usomm_wallet_reward_map[k] = int(v * USOMM_FACTOR)
+        self.usomm_wallet_reward_map = usomm_wallet_reward_map
+
+        self.usomm_json_save_path.parent.mkdir(exist_ok=True, parents=True)
+
+        with open(self.usomm_json_save_path, 'w') as fp:
+            json.dump(self.usomm_wallet_reward_map, fp)
 
 
 if __name__ == "__main__":
 
     osr = OsmosisSnapshotRewards()
     osr.compute_redistribution_amounts()
-    with open(Path('../token_rewards/osmosis_pool_rewards.json').resolve()) as f:
+    osr.convert_to_usomm()
+    with open(
+            Path(
+                '../token_rewards/old_rewards_in_units_somm/osmosis_pool_rewards.json'
+            ).resolve()) as f:
         wallet_reward_map: Dict[Wallet, float] = json.load(f)
         total_rewards: float = sum(wallet_reward_map.values())
-        print(f"Total airdrop rewards on Osmosis: {total_rewards}")
+        print(f"Total airdrop rewards on Osmosis in SOMM: {total_rewards}")
 
+    with open(
+            Path('../token_rewards/osmosis_pool_rewards_usomm.json').resolve()
+    ) as f:
+        usomm_wallet_reward_map: Dict[Wallet, float] = json.load(f)
+        total_rewards: float = sum(usomm_wallet_reward_map.values())
+        print(f"Total airdrop rewards on Osmosis in uSOMM: {total_rewards}")
